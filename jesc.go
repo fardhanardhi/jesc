ed138e1e49661e973218e66765d53b708914b9bf
fardhanardhi <fa.dsg.id@gmail.com>
2025-05-30 02:09:56 +0700

    Refactor command-line flag handling for improved readability and consistency

--- jesc.go
+++ jesc.go
@@ -12,15 +12,24 @@ import (
 	"github.com/spf13/pflag"
 )
 
+const formatFlagName, formatFlagSort = "format", "f"
+const fileFlagName = "file"
+const outputFlagName = "output"
+
 func main() {
 	start := time.Now()
 
-	format := pflag.BoolP("format", "f", false, "Format json with indentation")
-	filePath := pflag.String("file", "", "Input filepath")
+	fmt.Println("")
+
+	format := pflag.BoolP(formatFlagName, formatFlagSort, false, "Format json with indentation")
+	filePath := pflag.String(fileFlagName, "", "Input filepath")
+	outputPath := pflag.String(outputFlagName, "", "Output filepath")
+
 	pflag.Parse()
+
 	args := pflag.Args()
 
-	if *filePath != "" && len(args) > 0 {
+	if pflag.Lookup(fileFlagName).Changed && len(args) > 0 {
 		fmt.Println("Either pass json directly as inline argument or specify the file path using --file")
 		fmt.Println("")
 		os.Exit(1)
@@ -28,14 +37,15 @@ func main() {
 
 	var input string
 
-	if *filePath != "" {
+	if pflag.Lookup(fileFlagName).Changed {
 		// Open our jsonFile
 		jsonFile, err := os.Open(*filePath)
 		// if we os.Open returns an error then handle it
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Printf("\nSuccessfully Opened %s", *filePath)
+		fmt.Printf("Successfully opened %s\n", *filePath)
+		fmt.Println("")
 		// defer the closing of our jsonFile so that we can parse it later on
 		defer jsonFile.Close()
 		// read our opened xmlFile as a byte array.
@@ -48,20 +58,28 @@ func main() {
 	var target map[string]any
 	err := json.Unmarshal([]byte(input), &target)
 	if err != nil {
-		log.Fatalf("Unable to marshal JSON due to %s", err)
+		log.Fatalf("Unable to marshal JSON due to %s\n", err)
 	}
 	var data = recusiveFormat(target)
 	var jsonBytes []byte
 	if *format {
-		jsonBytes, _ = json.MarshalIndent(data, "", "    ")
+		jsonBytes, _ = json.MarshalIndent(data, "", "  ")
 	} else {
 		jsonBytes, _ = json.Marshal(data)
 	}
-	fmt.Println("")
-	fmt.Println("")
-	fmt.Println("[INFO] Result:")
-	fmt.Println("")
-	fmt.Println(string(jsonBytes))
+	if pflag.Lookup(outputFlagName).Changed {
+		err = os.WriteFile(*outputPath, jsonBytes, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error writing JSON to file:", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("Successfully write on %s\n", *outputPath)
+		}
+	} else {
+		fmt.Println("[INFO] Result:")
+		fmt.Println("")
+		fmt.Println(string(jsonBytes))
+	}
 	fmt.Println("")
 	fmt.Println("[INFO] JSON successfuly escaped ✨")
 	fmt.Printf("[INFO] Execution took %s\n", time.Since(start))
